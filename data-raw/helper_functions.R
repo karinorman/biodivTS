92c5082a857f3e821baf0586021fd8c2345d4b80
Kari Norman <kari@normanfamily.org>
2019-07-03 11:47:23 -0700

    Make function for chacking names missing ID's against other providers to get synonyms that might have ids.

--- data-raw/helper_functions.R
+++ data-raw/helper_functions.R
@@ -28,3 +28,25 @@ get_dupe_ids <- function(data, orig_id){
     filter(n()>1) %>%
     ungroup()
 }
+
+#Check if synonyms from other providers have matches in the original database
+match_providers <- function(data, original_provider) {
+  unmatched <- data %>% filter(is.na(id)) %>% pull(sourceName) %>% unique()
+  providers <- c("itis", "ncbi", "col", "gbif", "fb", "wd", "ott", "iucn")
+
+  #match all the un-ID'd names to other providers, check if synonyms given by those providers match known ITIS species
+  alt_names <-map_df(providers[providers != original_provider], function(name) synonyms(unmatched, name)) %>%
+    drop_na(acceptedNameUsageID) %>%
+    #new matches can be in either the synonym or acceptedNameUsage column, so let's combine them
+    gather(type, altProviderName, synonym, acceptedNameUsage) %>%
+    select(altProviderName, input) %>%
+    distinct() %>%
+    drop_na(altProviderName)
+
+  #match accepted names from
+  syn_res <- by_name(na.omit(unique(alt_names$altProviderName)), original_provider) %>%
+    drop_na(acceptedNameUsageID) %>%
+    rename(altProviderName = input) %>%
+    left_join(alt_names, na_matches = "never", by = "altProviderName")
+
+}
