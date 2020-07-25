e2c5645085dc95af67c28876a6bf7c78b1034d8f
karinorman <kari@normanfamily.org>
2020-07-25 15:53:04 +0000

    Add github_pat

--- analysis/pins_initialize.R
+++ analysis/pins_initialize.R
@@ -7,6 +7,10 @@ getOption("pins.github.release") == 0
 #if above expression is FALSE, change .Rprofile option to (usethis::edit_r_profile):
 #options(pins.github.release = 0)
 
+#make sure the github PAT is set
+#usethis::edit_r_environ()
+#GITHUB_PAT="1e03178a12e2c53fb5f640cb30f4473349be3b30"
+
 board_register_github(repo = "karinorman/biodivTS", branch = "master")
 
 #to pin:
